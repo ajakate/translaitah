f8cecff430b2c00e94032eb689f7e37d128e195b
ajakate <ajay.jakate@gmail.com>
2014-09-03 22:16:12 -0500

    adding integration tests for google

---
+++ spec/translaitah_integration_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Translaitah do
+
+  describe '#translate' do
+
+    it 'should return Hola for en -> es translation of hello' do
+
+      translator = Translaitah.new('en','es')
+      returned_translation = translator.translate('Hello')
+
+      expected_result = 'Hola'
+
+      expect(returned_translation).to eq(expected_result)
+    end
+
+    it 'should return I don\'t like you for fr -> en translation of the french way to say that' do
+
+      translator = Translaitah.new('fr','en')
+      returned_translation = translator.translate('Je ne vous aime pas bien')
+
+      expected_result = 'I do not like you'
+
+      expect(returned_translation).to eq(expected_result)
+    end
+  end
+end
