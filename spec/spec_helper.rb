54c1c1c0d1abb1d0239eea4fa325ca1e157c25cb
ajakate <ajay.jakate@gmail.com>
2014-09-03 22:14:58 -0500

    adding color and stdout for rspec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,14 @@ require 'translaitah'
 # end with _spec.rb. You can configure this pattern with with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 # Dir[File.expand_path('spec/support/**/*.rb')].each(&method(:require)) #{ |f| require f }
+RSpec.configure do |config|
+  # Spec Filtering
+ # config.filter_run focus: true
+ # config.run_all_when_everything_filtered = true
+#  config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  # Output
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
+end
